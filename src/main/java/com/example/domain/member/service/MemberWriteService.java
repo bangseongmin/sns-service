ad18d2a0f3617e623d63b8ad0988befcb6bea79e
bang-star <bangseongmin@naver.com>
2023-05-04 02:17:41 +0900

    #3 - 회원정보 조회 구현 (#9)
    1. 회원등록 메소드를 컨트롤러와 서비스 메소드명 통일
    2. MapSqlParameterSource, RowMapper 등을 이용하여 멤버 조회 기능 구현
    3. 데이터베이스의 데이터의 안전성을 위해 Dto로 변환하여 반환

--- src/main/java/com/example/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/domain/member/service/MemberWriteService.java
@@ -1,10 +1,13 @@
 package com.example.domain.member.service;
 
+import com.example.domain.member.dto.MemberDto;
 import com.example.domain.member.dto.RegisterMemberCommand;
 import com.example.domain.member.entity.Member;
 import com.example.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.example.domain.member.dto.MemberDto.toDto;
 
 @RequiredArgsConstructor
 @Service
@@ -12,7 +15,7 @@ public class MemberWriteService {
 
     private final MemberRepository memberRepository;
 
-    public Member create(RegisterMemberCommand command) {
+    public MemberDto register(RegisterMemberCommand command) {
         /**
          * 목표 - 회원정보(이메일, 닉네임, 생년월일) 등록
          *     - 닉네임은 10자를 넘길 수 없다.
@@ -28,6 +31,8 @@ public class MemberWriteService {
                 .birthday(command.birthday())
                 .build();
 
-        return memberRepository.save(member);
+        Member saveMember = memberRepository.save(member);
+
+        return toDto(saveMember);
     }
 }
