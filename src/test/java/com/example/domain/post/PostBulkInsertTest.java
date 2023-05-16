aaebd40b3cf26dad818907b957fe3e5c627303df
bang-star <bangseongmin@naver.com>
2023-05-16 23:53:34 +0900

    #12 - 성능 테스트를 위한 게시물 벌그 인서트 구현
    EasyRandom 을 이용하여 100만건에 데이터를 원초적인 방법으로 삽입
    
    * https://github.com/j-easy/easy-random/wiki

---
+++ src/test/java/com/example/domain/post/PostBulkInsertTest.java
@@ -0,0 +1,47 @@
+package com.example.domain.post;
+
+import com.example.domain.post.entity.Post;
+import com.example.domain.post.repository.PostRepository;
+import com.example.util.PostFixtureFactory;
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class PostBulkInsertTest {
+
+    @Autowired private PostRepository postRepository;
+
+    @Test
+    void bulkInsert() {
+        EasyRandom easyRandom = PostFixtureFactory.get(
+                3L,
+                LocalDate.of(2023, 1, 1),
+                LocalDate.of(2023, 1, 1)
+        );
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        List<Post> posts = IntStream.range(0, 10000 * 100)
+                .parallel()
+                .mapToObj(i -> easyRandom.nextObject(Post.class))
+                .toList();
+
+        stopWatch.stop();
+        System.out.println("객체 생성 시간: " + stopWatch.getTotalTimeSeconds());
+
+        StopWatch queryStopWatch = new StopWatch();
+        queryStopWatch.start();
+        postRepository.bulkInsert(posts);
+        queryStopWatch.stop();
+
+        System.out.println("DB INSERT 시간: " + queryStopWatch.getTotalTimeSeconds());
+    }
+}
