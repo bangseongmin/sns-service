ac63eee1e269351be3d2b3e28d03639e20758a2c
bang-star <bangseongmin@naver.com>
2023-05-20 16:24:27 +0900

    #15 - 오프셋기반 페이지네이션

--- src/main/java/com/example/application/controller/PostController.java
+++ src/main/java/com/example/application/controller/PostController.java
@@ -3,9 +3,13 @@ package com.example.application.controller;
 import com.example.domain.post.dto.DailyPostCount;
 import com.example.domain.post.dto.DailyPostCountRequest;
 import com.example.domain.post.dto.PostCommand;
+import com.example.domain.post.entity.Post;
 import com.example.domain.post.service.PostReadService;
 import com.example.domain.post.service.PostWriteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,5 +30,13 @@ public class PostController {
     @GetMapping("/daily-post-counts")
     public List<DailyPostCount> getDailyPostCounts(DailyPostCountRequest request) {
         return postReadService.getDailyPostCount(request);
+    }
+
+    @GetMapping("/members/{memberId}")
+    public Page<Post> getPosts(
+            @PathVariable Long memberId,
+            Pageable pageable
+    ) {
+        return postReadService.getPosts(memberId, pageable);
     }
 }
