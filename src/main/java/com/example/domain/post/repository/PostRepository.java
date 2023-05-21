55a24a922f984b2eb01dbb86dd13b0f9be12eee7
bang-star <bangseongmin@naver.com>
2023-05-21 12:16:35 +0900

    #19 - 타임라인 구현
    타임라인은 커서 기반 페이지네이션 방식 적용

--- src/main/java/com/example/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/domain/post/repository/PostRepository.java
@@ -145,6 +145,26 @@ public class PostRepository {
         return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
     }
 
+    public List<Post> findAllByInMemberIdAndOrderByIdDesc(List<Long> memberIds, int size) {
+        if(memberIds.isEmpty()) {
+            return List.of();
+        }
+
+        String sql = String.format("""
+                SELECT *
+                FROM %s
+                WHERE memberId in (:memberIds)
+                ORDER BY id desc
+                LIMIT :size
+                """, TABLE);
+
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("memberIds", memberIds)
+                .addValue("size", size);
+
+        return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+    }
+
     public List<Post> findAllByLessThanIdAndMemberIdAndOrderByIdDesc(Long id, Long memberId, int size) {
         String sql = String.format("""
                 SELECT *
@@ -156,6 +176,27 @@ public class PostRepository {
 
         MapSqlParameterSource params = new MapSqlParameterSource()
                 .addValue("memberId", memberId)
+                .addValue("id", id)
+                .addValue("size", size);
+
+        return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+    }
+
+    public List<Post> findAllByLessThanIdAndInMemberIdsAndOrderByIdDesc(Long id, List<Long> memberIds, int size) {
+        if(memberIds.isEmpty()) {
+            return List.of();
+        }
+
+        String sql = String.format("""
+                SELECT *
+                FROM %s
+                WHERE memberId in (:memberIds) and id < :id
+                ORDER BY id desc
+                LIMIT :size
+                """, TABLE);
+
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("memberIds", memberIds)
                 .addValue("id", id)
                 .addValue("size", size);
 
